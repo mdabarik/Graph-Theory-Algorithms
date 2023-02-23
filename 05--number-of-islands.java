ea22240b4f943500380165a24ca199526591ab5f
Md. A. Barik <48405620+mdabarik@users.noreply.github.com>
2023-02-23 11:48:45 +0600

    Create 05--number-of-islands.java

---
+++ 05--number-of-islands.java
@@ -0,0 +1,49 @@
+/*
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3 */
+
+
+class Solution {
+    public static void dfs(char[][] grid, int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == '0')
+            return;
+        grid[r][c] = '0';
+        // down, up, right, left
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+    
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                if (grid[r][c] == '1') {
+                    count += 1;
+                    dfs(grid, r, c);
+                }
+            }
+        } 
+        return count;
+    }
+} // TC: O(m * n), SC: O(1)
